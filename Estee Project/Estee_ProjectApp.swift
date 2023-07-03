1fcfc685c698618dfa0f97eca43c37fb3d21a2df
Kate <kmarine2024@pingry.org>
2023-07-03 08:49:27 -0400

    Initial Commit

---
+++ Estee Project/Estee_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Estee_ProjectApp.swift
+//  Estee Project
+//
+//  Created by Kate Marine on 7/3/23.
+//
+
+import SwiftUI
+
+@main
+struct Estee_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
